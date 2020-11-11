140a7dc9a73f838ce00d077f463f5808e3569034
Nanduuuu <68100034+Nanduuuu@users.noreply.github.com>
2020-11-11 12:58:31 +0530

    Update purge-radacct.sql

--- purge-radacct.sql
+++ purge-radacct.sql
@@ -10,28 +10,27 @@ purge_code:begin
     DECLARE live_comment varchar(200) default 'logging initiated';
     -- Handler Condition
     DECLARE lock_wait_err condition for 1205 ;
-    DECLARE deadlock_err condition for sqlstate '40001';
+    DECLARE deadlock_err condition for 1213;
     -- Handler Declaration
     DECLARE exit handler for lock_wait_err -- Error: 1205 SQLSTATE: HY000 (ER_LOCK_WAIT_TIMEOUT)
         begin
-        set dml_action = 'rolled back'; set live_comment = 'Transaction Rolled Back';
+        set dml_action = 'rolled back'; set live_comment = 'lock wait happened: transaction rolled back';
         update radius13.logs set stop_time=NOW(), activity_time=timediff(stop_time,start_time),
         status=dml_action, comment=live_comment where log_tbl_id=last_inserted_id;
         rollback ;
         end ;
-    DECLARE exit handler for deadlock_err -- Error 40001 SQLSTATE: HY000 (ER_LOCK_DEADLOCK)
+    DECLARE exit handler for deadlock_err -- Error 1213 SQLSTATE 40001: HY000 (ER_LOCK_DEADLOCK)
         begin
-            set dml_action = 'rolled back'; set live_comment = 'deadlock found';
+            set dml_action = 'rolled back'; set live_comment = 'deadlock found; transaction rolled back';
             update radius13.logs set stop_time=NOW(), activity_time=timediff(stop_time,start_time),
             status=dml_action, comment=live_comment where log_tbl_id=last_inserted_id;
         end ;
     -- logging
-    insert into radius13.logs (log_head,query_type,start_time,status,comment) values 
+    insert into radius13.logs (log_head,query_type,start_time,status,comment) values
                               ('Purge Radacct Table','delete',NOW(),dml_action,live_comment);
-    -- find last inserted id in log table.
     select last_insert_id() into last_inserted_id;
     -- find radacctid and store into variable radacct_id , using radacctid found to calculate rows to delete;
-    start transaction ;
+    START TRANSACTION ;
     select radacctid into radacct_id from radius13.radacct where acctstoptime < (NOW() - interval 90 DAY ) and
            acctstoptime is not null order by radacctid desc limit 1 ;
     select count(*) into row_count from radius13.radacct where acctstoptime is not null and radacctid <= radacct_id;
@@ -39,7 +38,7 @@ purge_code:begin
     -- logging
     update radius13.logs set stop_time=NOW(), rows_affected=row_count, activity_time=timediff(stop_time,start_time),
                              comment=live_comment where log_tbl_id=last_inserted_id;
-    commit ;
+    COMMIT ;
     -- purge operation on radacct table using found radacct_id and row_count
     -- looping starts
     looper_deletes : loop
