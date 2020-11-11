1335f434b607e10773883f8095090b8b76dfa8af
Nanduuuu <68100034+Nanduuuu@users.noreply.github.com>
2020-11-11 10:47:40 +0530

    Update create-log-table.sql

--- create-log-table.sql
+++ create-log-table.sql
@@ -1,4 +1,4 @@
-#Create log record table
+-- Create log record table
 Create table `logs` ( `log_tbl_id` int unsigned NOT NULL AUTO_INCREMENT, `log_head` text, `dml_query` enum('delete','update','alter','insert') DEFAULT NULL,
 `start_time` datetime DEFAULT NULL, `stop_time` datetime DEFAULT NULL, `activity_time` time DEFAULT '00:00:00', `rows_affected` int DEFAULT NULL,
 `status` enum('started','locked','continuing','rolled back','completed','canceled','failed') DEFAULT NULL, `comment` text, PRIMARY KEY (`log_tbl_id`)) ENGINE=InnoDB
